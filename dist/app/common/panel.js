5da1d0daa00ebd7c417423d23ef599663f83ec88
Roy Arad <royar13@gmail.com>
2016-05-06 12:16:38 +0300

    Create basic design

---
+++ dist/app/common/panel.js
@@ -0,0 +1,9 @@
+angular.module('library', []).controller('panelCtrl', function ($scope) {
+    var bgRatio = 1.67;
+    var bgWidth = $(window).height() * bgRatio;
+    var leftMargin = ($(window).width() - bgWidth) / 2;
+    $scope.panelStyle = {};
+    $scope.panelStyle.right = leftMargin + 20;
+    $scope.panelStyle.width = bgWidth * 0.7;
+    $scope.panelStyle.height = $(window).height() * 0.8;
+});
